146e2541002451738978944618868415e89c829b
Gregory Nitch <nitch.gregory@outlook.com>
2024-06-03 09:52:23 +0900

    Completed map coordinate class and gets for design outputs

--- dungeon/src/designs/MapDesign.java
+++ dungeon/src/designs/MapDesign.java
@@ -2,18 +2,33 @@ package designs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.SortedSet;
+import models.MapCoordinate;
 
 public class MapDesign {
 
     private ArrayList<ArrayList<ArrayList<String>>> connectionMatrix;
-    private SortedSet<int[]> seededSections;
-    private SortedSet<int[]> safetyBuffer;
+    private SortedSet<MapCoordinate> seededSections;
+    private SortedSet<MapCoordinate> safetyBuffer;
 
     // Main outputs
     private ArrayList<ArrayList<Integer>> displayMatrix;
     private ArrayList<ArrayList<ArrayList<Integer>>> validIdMatrix;
     private HashMap<Integer, String> imageMap = new HashMap<>();
+
+    public List<ArrayList<Integer>> getDisplayMatrix() {
+        return displayMatrix;
+    }
+
+    public List<ArrayList<ArrayList<Integer>>> getValidIdMatrix() {
+        return validIdMatrix;
+    }
+
+    public Map<Integer, String> getImageMap() {
+        return imageMap;
+    }
 
     @Override
     public String toString() {
