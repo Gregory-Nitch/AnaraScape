f41c51677fd7c1b7a5711dff97fd0b10b9d974d9
Gregory Nitch <nitch.gregory@outlook.com>
2024-06-14 20:58:48 +0900

    Revert "Completed map coordinate class and gets for design outputs"
    This reverts commit 146e2541002451738978944618868415e89c829b.

--- dungeon/src/designs/MapDesign.java
+++ dungeon/src/designs/MapDesign.java
@@ -2,33 +2,18 @@ package designs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.SortedSet;
-import models.MapCoordinate;
 
 public class MapDesign {
 
     private ArrayList<ArrayList<ArrayList<String>>> connectionMatrix;
-    private SortedSet<MapCoordinate> seededSections;
-    private SortedSet<MapCoordinate> safetyBuffer;
+    private SortedSet<int[]> seededSections;
+    private SortedSet<int[]> safetyBuffer;
 
     // Main outputs
     ArrayList<ArrayList<Integer>> displayMatrix;
     ArrayList<ArrayList<ArrayList<Integer>>> validIdMatrix;
     HashMap<Integer, String> imageMap = new HashMap<>();
-
-    public List<ArrayList<Integer>> getDisplayMatrix() {
-        return displayMatrix;
-    }
-
-    public List<ArrayList<ArrayList<Integer>>> getValidIdMatrix() {
-        return validIdMatrix;
-    }
-
-    public Map<Integer, String> getImageMap() {
-        return imageMap;
-    }
 
     @Override
     public String toString() {
